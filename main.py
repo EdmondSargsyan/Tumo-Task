13474a4de082d9b72cb1c653c957fb891081144e
Edmond <sargsyan-norik@inbox.ru>
2023-06-23 21:59:58 +0400

    Task

--- main.py
+++ main.py
@@ -1,6 +1,28 @@
-def function(num):
-    if num % 2 == 0:
-        print(f"{num} zuyg")
-    else:
-        print(f"{num} kent")
-function(int(input("Input number: ")))
+
+class BankAccount:
+    def __init__(self,account_number, summ):
+        self.account_number = account_number
+        self.summ = summ
+    def deposit(self,a):
+        self.summ = self.summ + a
+        return self.summ
+    def withdraw(self,a):
+        self.summ = self.summ - a
+        return self.summ 
+    def get_balance(self):
+        return self.summ
+        
+
+account =  BankAccount('12',2000.0) 
+
+
+print(account.summ)
+print(account.account_number)
+print(account.deposit(15.0))
+print(account.withdraw(5.0))
+balance = account.get_balance()
+print(f'Current balance:${balance}')
+
+
+
+        
